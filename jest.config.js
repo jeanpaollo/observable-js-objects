510dc21278e8564df14bdd4502070fbe67ecf4b6
Jean Paollo Olimpio Gomes Ferreira <jeanpaollo@gmail.com>
2024-09-22 13:26:10 -0300

    1.1.2

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+  coveragePathIgnorePatterns: ["/dist/"],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * from "./functions";
 export * from "./implementations";
 export * from "./interfaces";
